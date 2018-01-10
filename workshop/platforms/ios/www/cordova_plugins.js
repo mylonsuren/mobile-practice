9674f2b0e041598a8a3be4a36f6e56378012858b
Mylon <mylonsuren@yahoo.ca>
2018-01-10 10:34:14 -0500

    reduced click delay

--- workshop/platforms/ios/www/cordova_plugins.js
+++ workshop/platforms/ios/www/cordova_plugins.js
@@ -7,13 +7,22 @@ module.exports = [
     "clobbers": [
       "device"
     ]
+  },
+  {
+    "id": "cordova-plugin-dialogs.notification",
+    "file": "plugins/cordova-plugin-dialogs/www/notification.js",
+    "pluginId": "cordova-plugin-dialogs",
+    "merges": [
+      "navigator.notification"
+    ]
   }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
   "cordova-plugin-whitelist": "1.3.3",
-  "cordova-plugin-device": "2.0.1"
+  "cordova-plugin-device": "2.0.1",
+  "cordova-plugin-dialogs": "2.0.1"
 };
 // BOTTOM OF METADATA
 });
