6d2b3adaa768ba4957e6284f3b2466725fbe371e
Mylon <mylonsuren@yahoo.ca>
2018-01-10 13:12:32 -0500

    updated views

--- workshop/platforms/ios/www/js/app.js
+++ workshop/platforms/ios/www/js/app.js
@@ -2,44 +2,32 @@
 (function () {
 
     /* ---------------------------------- Local Variables ---------------------------------- */
+    HomeView.prototype.template = Handlebars.compile($("#home-tpl").html());
+    EmployeeListView.prototype.template = Handlebars.compile($("#employee-list-tpl").html());
+
     var service = new EmployeeService();
     service.initialize().done(function () {
-        console.log("Service initialized");
+        $('body').html(new HomeView(service).render().$el);
     });
 
     /* --------------------------------- Event Registration -------------------------------- */
-    $('.search-key').on('keyup', findByName);
-    $('.help-btn').on('click', function() {
-        alert("Employee Directory v3.4");
-    });
-
     document.addEventListener('deviceready', function () {
-
+        StatusBar.overlaysWebView( false );
+        StatusBar.backgroundColorByHexString('#ffffff');
+        StatusBar.styleDefault();
         FastClick.attach(document.body);
-
-      if (navigator.notification) { // Override default HTML alert with native dialog
-          window.alert = function (message) {
-              navigator.notification.alert(
-                  message,    // message
-                  null,       // callback
-                  "Workshop", // title
-                  'OK'        // buttonName
-              );
-          };
-      }
+        if (navigator.notification) { // Override default HTML alert with native dialog
+            window.alert = function (message) {
+                navigator.notification.alert(
+                    message,    // message
+                    null,       // callback
+                    "Workshop", // title
+                    'OK'        // buttonName
+                );
+            };
+        }
     }, false);
 
     /* ---------------------------------- Local Functions ---------------------------------- */
-    function findByName() {
-        service.findByName($('.search-key').val()).done(function (employees) {
-            var l = employees.length;
-            var e;
-            $('.employee-list').empty();
-            for (var i = 0; i < l; i++) {
-                e = employees[i];
-                $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
-            }
-        });
-    }
 
 }());
---
+++ workshop/www/js/HomeView.js
@@ -0,0 +1,23 @@
+// lines (16 sloc)  387 Bytes
+var EmployeeListView = function () {
+
+   var employees;
+
+   this.initialize = function() {
+       this.$el = $('<div/>');
+       this.render();
+   };
+
+   this.setEmployees = function(list) {
+       employees = list;
+       this.render();
+   }
+
+   this.render = function() {
+       this.$el.html(this.template(employees));
+       return this;
+   };
+
+   this.initialize();
+
+}
