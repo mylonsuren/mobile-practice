73725a4e500e0a2c3887dfa32c7a950013c5274f
Mylon <mylonsuren@yahoo.ca>
2018-01-11 16:02:36 -0500

    minor updates

---
+++ new-project/platforms/ios/www/cordova_plugins.js
@@ -0,0 +1,28 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+  {
+    "id": "cordova-plugin-dialogs.notification",
+    "file": "plugins/cordova-plugin-dialogs/www/notification.js",
+    "pluginId": "cordova-plugin-dialogs",
+    "merges": [
+      "navigator.notification"
+    ]
+  },
+  {
+    "id": "cordova-plugin-device.device",
+    "file": "plugins/cordova-plugin-device/www/device.js",
+    "pluginId": "cordova-plugin-device",
+    "clobbers": [
+      "device"
+    ]
+  }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+  "cordova-plugin-dialogs": "2.0.1",
+  "cordova-plugin-whitelist": "1.3.3",
+  "cordova-plugin-device": "2.0.1"
+};
+// BOTTOM OF METADATA
+});
